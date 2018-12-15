db160abcf960201dacde7fedbfedcc450205a82b
Varinder Singh <varindersingh271990@gmail.com>
2018-12-15 21:14:04 +0530

    user can update item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myresolutions';
+  public addResolution:any = {
+    name:""
+  };
+  public edit = -1;
+  resolutions:any = []; 
+
+  onKeyUp(event:any){
+    if(event.key=="Enter"){
+      if(this.edit>-1){//edit
+        this.updateItem(this.addResolution,this.edit);
+      }else{//add new
+        this.addItem(this.addResolution);
+      }
+      //this.addResolution.name = "";
+    }
+  }
+
+  editResolution(item,i){
+     this.addResolution = item;
+     this.edit = i; 
+  }
+
+  addItem(item){
+    this.resolutions.push(item);
+    console.log(this.resolutions);
+  }
+
+  updateItem(res,i){
+    this.addResolution;
+  }
+
 }
